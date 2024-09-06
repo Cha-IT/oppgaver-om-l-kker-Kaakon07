9eec4413cc776c8151002ebdbe131b15b3670bd5
Kaakon07 <haakonb2007@gmail.com>
2024-09-06 12:56:06 +0200

    vises nå på siden

--- .github/filer/script.js
+++ .github/filer/script.js
@@ -1,26 +1,32 @@
 let correctPass = false
 
 function whileLøkke() {
-    let num = 0
-    while (num <= 10) {
-        console.log(num)
-        num++
+    let whileLoop = document.getElementById("while");
+    let i = 0;
+    while (i <= 10) {
+        let currentI = i;  // Lag en kopi av 'i' for hver iterasjon
+        setTimeout(function(){
+            whileLoop.innerHTML += currentI + " ";
+        }, 500 * i);
+        i++;
     }
-    console.log("while")
-
-    
 }
+    
+
 
 function forLøkke() {
-    for (i = 0; i <= 10; i++) {
-        console.log(i)
+    let forLoop = document.getElementById("for")
+    for (let i = 0; i <= 10; i++) {
+        setTimeout(function(){
+            forLoop.innerHTML += i + " ";
+        },500 * i)
     }
     console.log("for")
 }
 
 
 function checkPass() {
-    let password = "balls"
+    let password = "balls" 
     let attemptedPass = prompt("skriv passord")
     if (password === attemptedPass) {
         return true
